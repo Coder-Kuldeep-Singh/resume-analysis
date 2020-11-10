3580456d25746bc754ced47eacf6d5c2b23bf492
mavensingh <kuldeep.avsar@gmail.com>
2020-11-10 20:12:52 +0530

    added method to extract name,email,linkedin,github,phoneNumber from the resume

---
+++ pdfread.js
@@ -0,0 +1,74 @@
+var pdf = require('pdf-text-extract');
+let filePath = "../../../Downloads/Kuldeep-Singh.pdf" //Kuldeep-Singh.pdf
+// COPY+OF+COPY+OF+Resume-Kuldeep-Singh.pdf
+
+
+let myTrim = (x) => {
+    return x.replace(/^\s+|\s+$/gm, '');
+}
+
+function getNumbers(string) {
+    string = string.split(" ");
+    var int = "";
+    for (var i = 0; i < string.length; i++) {
+        if (isNaN(string[i]) == false) {
+            int += string[i];
+        }
+    }
+    return int;
+}
+
+pdf(filePath, {
+    splitPages: false
+}, (err, text) => {
+    if (err) {
+        console.dir(err);
+        return
+    }
+
+    let Resume = {};
+    // console.log(text[0]);
+    let rm = text[0].replace(" ", "").split("\n");
+    Resume['Name'] = myTrim(rm[0]);
+    // let Numbers = [];
+    var regex = new RegExp(
+        // "\\+?\\(?\\d*\\)? ?\\(?\\d+\\)?\\d*([\\s./-]\\d{2,})+",
+        "\\+?\\(?\\d*\\)? ?\\(?\\d+\\)?\\d*([\\s./-]?\\d{2,})+",
+        "g"
+    );
+
+    for (let idx = 0; idx < rm.length; idx++) {
+
+        if (rm[idx].includes('linkedin.com')) {
+            Resume['linkedin'] = myTrim(rm[idx]);
+        }
+
+        if (rm[idx].includes('@gmail.com')) {
+            let emailOnly = rm[idx].split(".com");
+            // console.log();
+            Resume['email'] = myTrim(emailOnly[0]);
+        }
+
+        // Method 2 to extract phone number and successful
+        let number = getNumbers(rm[idx])
+        if (number.length >= 10) {
+            Resume['number'] = myTrim(number);
+        };
+
+
+        // Method 1 to extract phone number but failed
+        // if (regex.exec(rm[idx])) {
+        //     console.log(rm[idx]);
+        //     Resume['number'] = myTrim(rm[idx]);
+        // }
+
+        if (rm[idx].includes('https://github.com')) {
+            Resume['github'] = myTrim(rm[idx]);
+        }
+
+
+
+    }
+    // console.log(Numbers);
+    console.log(Resume);
+});
